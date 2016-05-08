20195dc9c688a655d8017b07aad36eaa43c22268
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-05-08 14:13:44 +0300

    Fixed double copying of config file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,10 @@
 set -e
 
 if [ ! -e piwik.php ]; then
-	tar cf - --one-file-system -C /usr/src/piwik . | tar xf -
-    cp /etc/piwik/config.ini.php ./config/config.ini.php
-	chown -R www-data .
+    tar cf - --one-file-system -C /usr/src/piwik . | tar xf -
+    chown -R www-data .
 fi
 cp /etc/piwik/config.ini.php ./config/config.ini.php
-
 chown www-data:1000 ./config/config.ini.php
 
 exec "$@"
